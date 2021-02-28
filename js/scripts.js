0edd421ac8b26237822e8c09fb17c6e328bac729
tlpackus <tlpackus@gmail.com>
2021-02-27 19:52:03 -0800

    add Pizza.prototype.calcCost

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
 function Pizza (name, toppings, size) {
-  this.name = name;
-  this.topping = toppings;
-  this.size = size;
-  this.price = 3;
+  this.name = name;  //string
+  this.topping = toppings;  //array
+  this.size = size;  //string
+  this.price = 3;  //value
 }
+
+Pizza.prototype.calcCost = function() {
+  if (this.size === "medium") {
+    this.price += 2;
+  } else if (this.size === "large") {
+    this.price += 7;
+  } else if (this.size === "giganto") {
+    this.price += 12;
+  }
+  for (i = 0; i < this.topping.length; i++) {
+    this.price += 1;
+  }
+  return this.price;
+};
